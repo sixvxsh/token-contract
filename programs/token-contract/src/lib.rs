37d652554effa26d2e8937930902319f358a01ce
sixvxsh <sixvxsh.j@gmail.com>
2023-05-09 13:04:36 +0330

    conf dev dependencies

--- programs/token-contract/src/lib.rs
+++ programs/token-contract/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token;
-use anchor_spl::token::{Token, InitializeMint, MintTo, Transfer};
+use anchor_spl::token::{Token, MintTo, Transfer};
+
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -13,15 +14,34 @@ pub mod token_contract {
         let cpi_accounts = MintTo {
             mint: ctx.accounts.mint.to_account_info(),
             to: ctx.accounts.token_account.to_account_info(),
-            authority: ctx.accounts.payer.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
         };
-
+        
         let cpi_program = ctx.accounts.token_program.to_account_info();
-        // create the Cpicontext we need for the request
+        // Create the CpiContext we need for the request
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
-        //Execute anchor's helper function to mint tokens
+        // Execute anchor's helper function to mint tokens
         token::mint_to(cpi_ctx, 10)?;
+        
+        Ok(())
+    }
+
+    pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
+        // Create the Transfer struct for our context
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.from_authority.to_account_info(),
+        };
+         
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the Context for our Transfer request
+        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
+
+        // Execute anchor's helper function to transfer tokens
+        anchor_spl::token::transfer(cpi_ctx, 5)?;
+ 
         Ok(())
     }
 
@@ -29,13 +49,16 @@ pub mod token_contract {
 
 #[derive(Accounts)]
 pub struct MintToken<'info> {
+    /// CHECK: This is the token that we want to mint
     #[account(mut)]
     pub mint: UncheckedAccount<'info>,
     pub token_program: Program<'info, Token>,
+    /// CHECK: This is the token account that we want to mint tokens to
     #[account(mut)]
     pub token_account: UncheckedAccount<'info>,
+    /// CHECK: the authority of the mint account
     #[account(mut)]
-    pub payer: AccountInfo<'info>,
+    pub authority: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
